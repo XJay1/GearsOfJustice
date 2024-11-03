328a6515ef5140f6783bdca9c2fa126f7714fe54
Егений Крашаница <yevhen.krashanytsia@nure.ua>
2024-11-03 14:26:13 +0200

    morality test

--- GearsOfJustice/Source/GearsOfJustice/GearsOfJustice.Build.cs
+++ GearsOfJustice/Source/GearsOfJustice/GearsOfJustice.Build.cs
@@ -1,20 +1,13 @@
-﻿// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
 public class GearsOfJustice : ModuleRules
 {
-    public GearsOfJustice(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	public GearsOfJustice(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
-        PublicIncludePaths.AddRange(new string[]
-        {
-            "GearsOfJustice/Public",
-            "GearsOfJustice/Public/Menu",
-            "GearsOfJustice/Public/Menu/UI"
-        });
-
     }
 }
